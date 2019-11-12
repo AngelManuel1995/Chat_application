41d33045d9b66140d5122813caf0226f782c6945
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-11-11 22:40:13 -0500

    Serving the cliente

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT
+
+const publicFolder = path.join(__dirname, '..', 'public')
+
 const app = express()
+app.use(express.static(publicFolder))
 
 app.listen(PORT,() => {
     console.log('Port is up and running on port:'.toUpperCase(), + PORT)
