61e61294c00eb4c0cdd64e5d8469a7ac2d3423c1
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-11-13 00:09:38 -0500

    Login page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,11 +3,15 @@ const socket = io()
 let messages = []
 socket.on('enterChat', (messageEmmited) => {
     messages.push(messageEmmited)
-    document.querySelector('#messages').innerHTML = ''
+    document.querySelector('#messages__my').innerHTML = ''
     messages.forEach((message, index) => {
         let messageP = document.createElement('P')
+        let messageDiv = document.createElement('DIV')
+        messageDiv.classList.add('msg_sent')
+        messageDiv.classList.add('messages')
+        messageDiv.append(messageP)
         messageP.textContent = `${index + 1}. ${message}`
-        document.querySelector('#messages').append(messageP)
+        document.querySelector('#messages__my').append(messageDiv)
     })
 
 })
