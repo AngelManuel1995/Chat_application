e6df308256393d8268331cb6122467adee3bb5fe
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2019-11-14 19:44:44 -0500

    Validating userName and message

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -18,6 +18,28 @@ socket.on('message', (message) => {
 
 document.querySelector('#testForm').addEventListener('submit', (e) => {
     e.preventDefault()
+    if(document.querySelector('#userName').value === ''){
+        document.querySelector('#messages__my').innerHTML = ''
+        let messageP = document.createElement('P')
+        let messageDiv = document.createElement('DIV')
+        messageDiv.classList.add('msg_sent')
+        messageDiv.classList.add('messages')
+        messageDiv.append(messageP)
+        messageP.textContent = 'Admin says: Please provide a user name'
+        document.querySelector('#messages__my').append(messageDiv)
+        return
+    }
+    if(e.target.elements.message.value === ''){
+        document.querySelector('#messages__my').innerHTML = ''
+        let messageP = document.createElement('P')
+        let messageDiv = document.createElement('DIV')
+        messageDiv.classList.add('msg_sent')
+        messageDiv.classList.add('messages')
+        messageDiv.append(messageP)
+        messageP.textContent = 'Admin says: Please provide a message'
+        document.querySelector('#messages__my').append(messageDiv)
+        return
+    }
     const message = e.target.elements.message.value
     socket.emit('sendMessage', `${document.querySelector('#userName').value} says: ${message}`)
     e.target.elements.message.value = ''
